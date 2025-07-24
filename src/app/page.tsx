7d64ce31c94a371ebface9c0bdb34f29fea30239
@SandroHvex <sandro.luiz@hvex.com.br>
2025-07-24 00:10:27 -0300

    fix: update page structure to use <main> element for better semantics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
+import { Content } from "@/screens/landingPage/content"
 import { Header } from "@/screens/landingPage/header"
 import { Hero } from "@/screens/landingPage/hero"
 
 export default function Page() {
   return (
-    <div className="flex flex-col w-full px-4 h-screen">
+    <main className="flex flex-col w-full px-4 h-screen gap-6">
         <Header />
         <Hero />
-
-    </div>
+        <Content />
+    </main>
   )
 }
