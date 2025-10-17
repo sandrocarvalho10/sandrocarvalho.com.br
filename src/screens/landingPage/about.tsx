d3ccdba8d03135c5d7edb21e8da5ac32b8da6d20
Sandro Carvalho <sandro.luiz@hvex.com.br>
2025-10-17 00:56:22 -0300

    feat(landingPage): replace hero section with 3D avatar model
    add 3D avatar model using react-three-fiber for more engaging hero section
    create new about page component with existing animated cards

---
+++ src/screens/landingPage/about.tsx
@@ -0,0 +1,11 @@
+import { CardAnimated } from "../components/card-animated";
+import { CardAnimatedAvatar } from "../components/card-animated-avatar";
+
+export function About() {
+    return (<section className="w-full grid place-items-center h-full">
+        <div className="w-full max-w-7xl xs:pt-14 xs:grid xs:grid-cols-1 xs:gap-4 md:grid md:grid-cols-[3fr_1fr] md:gap-8 ">
+            <CardAnimated />
+            <CardAnimatedAvatar />
+        </div>
+    </section>)
+}
